f1125b4de7b7beace492fc4e81f71ae0f64b8b8d
fwj <123@qq.com>
2017-07-27 19:29:43 +0800

    test

---
+++ zhihu_login_requests.py
@@ -0,0 +1,10 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'FWJ'
+__date__ = 2017 / 7 / 27
+import requests
+try:
+    # python2
+    import cookielib
+except:
+    # python3
+    import http.cookiejar as cookielib
